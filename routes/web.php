fe58e0db4a92953583a2f800ff34fedbd5716edf
fixable11 <fixable11@gmail.com>
2019-01-13 15:28:37 +0200

    Routes cleaning up

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController');
 
+//Answers of the question
 Route::get('/questions/{slug}/answers', 'AnswersController@index')->name('answers.index');
 Route::post('/questions/{slug}/answers', 'AnswersController@store')->name('answers.store');
-//Route::get('questions/{slug}/answers/{answer}/edit', 'AnswersController@edit');
 Route::delete('questions/{slug}/answers/{answer}', 'AnswersController@destroy');
 Route::patch('questions/{slug}/answers/{answer}', 'AnswersController@update');
-Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show', 'edit']);
 
 //Accept answer
 Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
